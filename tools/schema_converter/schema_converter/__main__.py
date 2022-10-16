7ac11c91c76b9613bc838ff6a9c5e37571c5c637
adam <minge.adam@gmail.com>
2022-10-16 17:12:21 +0200

    add custom argparse types

--- tools/schema_converter/schema_converter/__main__.py
+++ tools/schema_converter/schema_converter/__main__.py
@@ -1,24 +1,64 @@
 """
 --------------------------------------------------------------
 schema_converter: command line tool for converting egnite
-schema files such as JSON,XML to Qt/QML interface files.
+schema files such as json,xml to Qt/QML interface files.
 --------------------------------------------------------------
-Usage: schema_converter <options> --sources <paths> --destination <directory>
+usage: schema_converter [-h] [--sources SOURCES [SOURCES ...]] [--destination DESTINATION]
 Options:
-  -h, --help             Show help screen only (this message).
+  -h, --help            show this help message and exit
+  --sources SOURCES [SOURCES ...]
+                        schema files such as json,xml to convert
+  --destination DESTINATION
+                        directory in which the generated files will be to add
 """
 
-from argparse import ArgumentParser
+from argparse import ArgumentParser, ArgumentTypeError
+from ast import parse
+from pathlib import Path
+
+
+def valid_source_paths(arg: str) -> Path:
+    """Custom argparse type for file path values given from the command line"""
+    path = Path(arg)
+
+    if not path.exists():
+        raise ArgumentTypeError(
+            f"given source path: {arg} doesn't exist")
+    if not path.is_file():
+        raise ArgumentTypeError(
+            f"given source path: {arg} isn't correct file path")
+
+    return path
+
+
+def valid_destination_path(arg: str) -> Path:
+    """Custom argparse type for dir path values given from the command line"""
+    path = Path(arg)
+
+    if not path.exists():
+        raise ArgumentTypeError(
+            f"given destination path: {arg} doesn't exist")
+    if not path.is_dir():
+        raise ArgumentTypeError(
+            f"given destination path: {arg} isn't correct dir path")
+
+    return path
+
+
+class Parser():
+    @staticmethod
+    def get_args():
+        parser = ArgumentParser(prog="schema_converter")
+        parser.add_argument("--sources", type=valid_source_paths, nargs="+", required=True,
+                            help="schema files such as json,xml to convert")
+        parser.add_argument("--destination", type=valid_destination_path, nargs=1, required=True,
+                            help="directory in which the generated files will be to add")
+        return parser.parse_args()
 
 
 def main():
-    parser = ArgumentParser(prog="RestGenerator")
-    parser.add_argument("--sources", type=str, nargs="+",
-                        help="")
-    parser.add_argument("--destination", nargs=1,
-                        help="")
-
-    args = parser.parse_args()
+    args = Parser.get_args()
+    print(f"args = ${args}")
 
 
 if __name__ == "__main__":
