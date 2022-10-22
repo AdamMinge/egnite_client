f3c654b365f448c3b7a6cffbdccb7cca006ee179
adam <minge.adam@gmail.com>
2022-10-22 11:24:23 +0200

    added basic schemas generation flow

--- tools/schema_converter/schema_converter/generator/qt_generator.py
+++ tools/schema_converter/schema_converter/generator/qt_generator.py
@@ -28,21 +28,39 @@ class SrcModel(generator.CppFile):
     pass
 
 class QtGenerator(Generator):
+    code_style = generator.CppCodeStyle(
+        indent=" "*2, 
+        lf="\n"
+    )
+    
     def _generate_client(self,
                          client_schema: ClientSchema,
                          header_stream: TextIOWrapper,
                          src_stream: TextIOWrapper,
                          include_dependencies: Iterable[Path]) -> None:
-        pass
+        header_client = HeaderClient()
+        src_client = SrcClient()
+        
+        header_stream.write(header_client.code(code_style=QtGenerator.code_style, indent_level=0))
+        src_stream.write(src_client.code(code_style=QtGenerator.code_style, indent_level=0))
+        
 
     def _generate_api(self, api_schema: ApiSchema,
                       header_stream: TextIOWrapper,
                       src_stream: TextIOWrapper,
                       include_dependencies: Iterable[Path]) -> None:
-        pass
+        header_api = HeaderApi()
+        src_api = SrcApi()
+        
+        header_stream.write(header_api.code(code_style=QtGenerator.code_style, indent_level=0))
+        src_stream.write(src_api.code(code_style=QtGenerator.code_style, indent_level=0))
 
     def _generate_model(self,
                         model_schema: ModelSchema,
                         header_stream: TextIOWrapper,
                         src_stream: TextIOWrapper) -> None:
-        pass
+        header_model = HeaderModel()
+        src_model = SrcModel()
+        
+        header_stream.write(header_model.code(code_style=QtGenerator.code_style, indent_level=0))
+        src_stream.write(src_model.code(code_style=QtGenerator.code_style, indent_level=0))
