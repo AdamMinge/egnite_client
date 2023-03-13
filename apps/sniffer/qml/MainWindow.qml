435b91a5866f7ea193a51727c6e6097624e161d6
adam <minge.adam@gmail.com>
2023-03-13 14:29:43 +0100

    added sniffer back

---
+++ apps/sniffer/qml/MainWindow.qml
@@ -0,0 +1,62 @@
+import QtQuick 2.15
+import QtQuick.Controls 2.15
+import QtQuick.Controls.Material 2.15
+
+import egnite.rest 1.0
+import egnite.auth 1.0
+
+import egnite.api 1.0
+import egnite.controls 1.0
+
+
+ApplicationWindow  {
+    id: root
+    minimumWidth: 800
+    minimumHeight: 700
+    visible: true
+
+    EgniteClient {
+        id: egnite_client
+
+        replyDecorator {
+            factories: [
+                QmlLoggerReplyFactory { logDetail: QmlLoggerReplyFactory.LogOnlyResult },
+                QmlJwtAuthenticatorReplyFactory { authenticator: egnite_authenticator }
+            ]
+        }
+    }
+
+    QmlJwtAuthenticator {
+        id: egnite_authenticator
+        client: egnite_client
+        path: "auth"
+
+        routing {
+            obtain: "login"
+            refresh: "login/refresh"
+            blacklist: "login/blacklist"
+        }
+
+        property bool isLogin: egnite_authenticator.accessToken != ""
+        property bool isLogout: !isLogin
+    }
+
+    RegistrationApi {
+        id: registration_api
+        client: egnite_client
+    }
+
+    AnimatedPagesView {
+        id: pages_view
+        anchors.fill: parent
+        currentItem: egnite_authenticator.isLogout ? auth_page : main_page
+
+        AuthPage {
+            id: auth_page
+        }
+
+        MainPage {
+            id: main_page
+        }
+    } 
+}
---
+++ apps/sniffer/src/main.cpp
@@ -0,0 +1,65 @@
+/* ---------------------------------- Egnite -------------------------------- */
+#include <egnite/cmd/parser.h>
+#include <egnite/config.h>
+/* ------------------------------------ Qt ---------------------------------- */
+#include <QFontDatabase>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickStyle>
+/* -------------------------------------------------------------------------- */
+
+/* ------------------------- initEnvironmentVariables ----------------------- */
+
+void initEnvironmentVariables() {
+  qputenv("QT_QUICK_CONTROLS_CONF", ":/viewer/qtquickcontrols2.conf");
+}
+
+/* ----------------------------- CommandLineParser -------------------------- */
+
+class ViewerCommandLineParser : public egnite::cmd::Parser {
+ public:
+  explicit ViewerCommandLineParser();
+  ~ViewerCommandLineParser() override;
+};
+
+ViewerCommandLineParser::ViewerCommandLineParser() = default;
+
+ViewerCommandLineParser::~ViewerCommandLineParser() = default;
+
+/* ----------------------------- parseCommandLine --------------------------- */
+
+static void parseCommandLine(QGuiApplication &app) {
+  ViewerCommandLineParser parser;
+  parser.process(app);
+}
+
+/* ------------------------------------ main -------------------------------- */
+
+int main(int argc, char **argv) {
+  initEnvironmentVariables();
+
+  QGuiApplication app(argc, argv);
+  QGuiApplication::setApplicationName(QStringLiteral("Egnite-Viewer"));
+  QGuiApplication::setApplicationVersion(QLatin1String(EGNITE_VERSION_STR));
+  QGuiApplication::setApplicationDisplayName(QStringLiteral("Egnite-Viewer"));
+  QGuiApplication::setOrganizationName(QStringLiteral("Egnite"));
+
+  parseCommandLine(app);
+
+  QQmlApplicationEngine engine;
+  const auto url = QUrl("qrc:/viewer/MainWindow.qml");
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl) QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
+
+  engine.addImportPath(app.applicationDirPath() + "/plugins");
+  engine.load(url);
+  if (engine.rootObjects().isEmpty()) {
+    QCoreApplication::exit(-1);
+  }
+
+  return QGuiApplication::exec();
+}
